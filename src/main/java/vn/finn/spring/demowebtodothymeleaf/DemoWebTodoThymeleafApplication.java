311401e28283a09eb4eed48831fac356a1876cca
finn-01 <nguyensam576@gmail.com>
2022-02-15 15:36:46 +0700

    set up

---
+++ src/main/java/vn/finn/spring/demowebtodothymeleaf/DemoWebTodoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demowebtodothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebTodoThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebTodoThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/demowebtodothymeleaf/DemoWebTodoThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demowebtodothymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebTodoThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
